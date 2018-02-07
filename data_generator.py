afbe34a1cef360d5b85ee38bed43ece0d64cb93a
nakatsuka <jacketgardensio@gmail.com>
2018-02-07 19:51:59 +0900

    initial commit

---
+++ data_generator.py
@@ -0,0 +1,61 @@
+import numpy as np
+from tensorflow.contrib.keras.python.keras.datasets import mnist
+from tensorflow.contrib.keras.python.keras.preprocessing.image import Iterator
+from tensorflow.contrib.keras.python.keras.utils import to_categorical
+
+
+class DataGenerator(Iterator):
+    def __init__(self, batch_size=64, shuffle=True, is_training=True, flatten=True):
+        (train_x, train_y), (test_x, test_y) = mnist.load_data()
+        if is_training:
+            self.x = train_x
+            self.y = train_y
+        else:
+            self.x = test_x
+            self.y = test_y
+        self.is_training = is_training
+        
+        if flatten:
+            # x: (N, 28, 28) → (N, 784)
+            self.x = self.x.reshape(self.x.shape[0], self.x.shape[1]*self.x.shape[2])
+        else:
+            # x: (N, 28, 28) → (N, 28, 28, 1)
+            self.x = np.expand_dims(self.x, axis=-1)
+        self.x = self.x / 255
+        # one-hot vectorize y
+        self.y = to_categorical(self.y, 10)
+        super().__init__(len(self.x), batch_size, shuffle, None)
+
+    def __call__(self, *args, **kwargs):
+        """
+        if is_training: return (image_batch, label_batch)
+        else          : return (generator yield  (image_batch, label_batch))
+        """
+        if self.is_training:
+            return self._flow_on_training()
+        else:
+            return self._flow_on_test()
+
+    def _flow_on_training(self):
+        # get minibatch indices
+        with self.lock:
+            index_array, current_index, current_batch_size = next(self.index_generator)
+        image_batch = self.x[index_array]
+        label_batch = self.y[index_array]
+        return image_batch, label_batch
+
+    def _flow_on_test(self):
+        # create indices
+        indexes = np.arange(self.n)
+
+        # calucuate steps per a test
+        steps = self.n // self.batch_size
+        if self.n % self.batch_size != 0:
+            steps += 1
+        
+        # yield loop
+        for i in range(steps):
+            index_array = indexes[i*self.batch_size: (i+1)*self.batch_size]
+            image_batch = self.x[index_array]
+            label_batch = self.y[index_array]
+            yield image_batch, label_batch
---
+++ model.py
@@ -0,0 +1,74 @@
+import os
+import tensorflow as tf
+from tensorflow.contrib.keras.python.keras.layers import Dense, Activation, Conv2D, MaxPool2D, Flatten
+from tensorflow.contrib.keras.python.keras.losses import categorical_crossentropy
+from tensorflow.contrib.keras.python.keras.metrics import categorical_accuracy
+
+
+class MLP:
+    def __init__(self,
+                 input_shape=(784, ),
+                 nb_classes=10,
+                 optimizer=tf.train.AdamOptimizer(1e-3)):
+        # create graph
+        self.input_ = tf.placeholder(tf.float32, [None] + list(input_shape))
+        self.logit, self.output = self.build(self.input_, nb_classes)
+        self.t = tf.placeholder(tf.float32, self.output.get_shape())
+        self.loss = tf.reduce_mean(categorical_crossentropy(self.t, self.output))
+        self.acc = tf.reduce_mean(categorical_accuracy(self.t, self.output))
+
+        self.optimizer = optimizer.minimize(self.loss)
+        self.saver = tf.train.Saver()
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def build(self, x, nb_classes):
+        _x = Conv2D(8, (3, 3), activation='relu')(x)
+        _x = MaxPool2D()(_x)
+        _x = Conv2D(16, (3, 3), activation='relu')(_x)
+        _x = MaxPool2D()(_x)
+        _x = Flatten()(_x)
+        _x = Dense(128, activation='relu')(_x)
+        logits = Dense(nb_classes, activation=None)(_x)
+        outputs = Activation('softmax')(logits)
+        return logits, outputs
+
+    def fit(self, data_generator, nb_epoch, model_dir):
+        batch_size = data_generator.batch_size
+        nb_sample = data_generator.n
+
+        # calucuate steps per a epoch
+        steps_per_epoch = nb_sample // batch_size
+        if nb_sample % batch_size != 0:
+            steps_per_epoch += 1
+
+        # fit loop
+        for epoch in range(1, nb_epoch+1):
+            print('\nepoch {} / {}'.format(epoch, nb_epoch))
+            for step in range(steps_per_epoch):
+                image_batch, label_batch = data_generator()
+                _, loss, acc = self.sess.run([self.optimizer, self.loss, self.acc],
+                                             feed_dict={self.input_: image_batch,
+                                                        self.t: label_batch})
+                print('{}/{}  loss : {:.4f}  acc : {:.4f}'.format(step, 
+                                                                  steps_per_epoch, 
+                                                                  loss, 
+                                                                  acc),
+                      end='\r')
+        self.save(model_dir)
+        print('\nTraining is done ... ')
+
+    def save(self, model_dir):
+        os.makedirs(model_dir, exist_ok=True)
+        self.saver.save(self.sess, os.path.join(model_dir, 'model.ckpt'))
+        
+    def evaluate_on_batch(self, x, y):
+        return self.sess.run(self.acc,
+                             feed_dict={self.input_: x,
+                                        self.t: y})
+    
+    def evaluate_generator(self, data_generator):
+        acc = 0
+        for x, y in data_generator():
+            acc += self.evaluate_on_batch(x, y) * len(x)
+        return acc / data_generator.n
